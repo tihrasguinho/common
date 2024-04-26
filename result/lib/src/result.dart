1b71ff044aff5821f3211a6c3288f6a39388b551
Tiago Alves <tiago.pessoal93@gmail.com>
2024-04-26 18:39:55 -0300

    result package

---
+++ result/lib/src/result.dart
@@ -0,0 +1,48 @@
+typedef AsyncResult<S extends Object?, E extends Object?> = Future<Result<S, E>>;
+
+sealed class Result<S extends Object?, E extends Object?> {
+  final S? _success;
+  final E? _error;
+
+  const Result(this._success, this._error);
+
+  bool get isSuccess => _success != null;
+
+  S? getSuccessOrNull() => _success;
+
+  S? getSuccessOrThrow() {
+    if (_success == null) throw Exception('Left value is null');
+
+    return _success;
+  }
+
+  S? getSuccessOrElse(S Function() orElse) => _success ?? orElse();
+
+  bool get isError => _error != null;
+
+  E? getErrorOrNull() => _error;
+
+  E? getErrorOrThrow() {
+    if (_error == null) throw Exception('Right value is null');
+
+    return _error;
+  }
+
+  E? getErrorOrElse(E Function() orElse) => _error ?? orElse();
+
+  T on<T>(T Function(S success) onSuccess, T Function(E error) onError) {
+    if (isSuccess) {
+      return onSuccess(getSuccessOrThrow() as S);
+    } else {
+      return onError(getErrorOrThrow() as E);
+    }
+  }
+}
+
+final class Success<S extends Object?, E extends Object?> extends Result<S, E> {
+  Success(S success) : super(success, null);
+}
+
+final class Error<S extends Object?, E extends Object?> extends Result<S, E> {
+  Error(E error) : super(null, error);
+}
